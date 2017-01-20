0c193948c6366a118ee259194e64daa42747e97c
William Petit <wpetit@cadoles.com>
2017-01-20 16:20:36 +0100

    Add script to send email via nodemailer

---
+++ scripts/test-smtp.js
@@ -0,0 +1,19 @@
+const nodemailer = require('nodemailer');
+const transporter = nodemailer.createTransport('smtp://john:doe@127.0.0.1:2525');
+
+// setup e-mail data with unicode symbols
+var mailOptions = {
+    from: '"Fred Foo ?" <foo@blurdybloop.com>', // sender address
+    to: 'bar@blurdybloop.com, baz@blurdybloop.com', // list of receivers
+    subject: 'Hello ✔', // Subject line
+    text: 'Hello world ?', // plaintext body
+    html: '<b>Hello world ?</b>' // html body
+};
+
+// send mail with defined transport object
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        return console.log(error);
+    }
+    console.log('Message sent: ' + info.response);
+});
